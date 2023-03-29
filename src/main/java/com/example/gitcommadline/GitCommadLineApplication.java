e339da8ffb46e2942d5e4afc46ae67f6b606712c
Thomas Lund Løvkilde <thom4a14@stud.kea.dk>
2023-03-29 14:39:12 +0200

    first commit + app.pro in .gitignore

---
+++ src/main/java/com/example/gitcommadline/GitCommadLineApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitcommadline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommadLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCommadLineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitcommadline/GitCommadLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcommadline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommadLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
